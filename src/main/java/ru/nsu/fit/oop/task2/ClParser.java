71e020d68e15a09d2d7c06414059db54d9da91c3
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-11 18:28:23 +0700

    Add command line parser

---
+++ src/main/java/ru/nsu/fit/oop/task2/ClParser.java
@@ -0,0 +1,67 @@
+package ru.nsu.fit.oop.task2;
+
+import org.apache.commons.cli.*;
+
+public class ClParser
+{
+    private CommandLine commandLine;
+
+    public boolean parse(String[] args)
+            throws ParseException
+    {
+        Options opts = new Options();
+        opts.addOption(Option.builder()
+                             .option("h")
+                             .longOpt("help")
+                             .desc("Print command help")
+                             .build())
+            .addOption(Option.builder()
+                             .option("i")
+                             .longOpt("input")
+                             .desc("Input file")
+                             .hasArg()
+                             .argName("path")
+                             .numberOfArgs(1)
+                             .type(String.class)
+                             .build())
+            .addOption(Option.builder()
+                             .option("o")
+                             .longOpt("output")
+                             .desc("Output csv file")
+                             .hasArg()
+                             .argName("path")
+                             .type(String.class)
+                             .build());
+
+        CommandLineParser clParser = new DefaultParser();
+        commandLine = clParser.parse(opts,
+                                     args);
+
+        if (commandLine.hasOption("help"))
+        {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("calc",
+                                    opts,
+                                    true);
+            return false;
+        }
+
+        return true;
+    }
+
+    public String getInput()
+    {
+        if (commandLine.hasOption("input"))
+            return commandLine.getOptionValue("input");
+        else
+            return null;
+    }
+
+    public String getOutput()
+    {
+        if (commandLine.hasOption("output"))
+            return commandLine.getOptionValue("output");
+        else
+            return null;
+    }
+}
