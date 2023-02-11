82a8bf579725bcf1dc599e3cc13db887620bc6f1
ptrvsrg <s.petrov1@g.nsu.ru>
2023-02-11 14:05:25 +0700

    Add push command

---
+++ src/main/java/ru/nsu/fit/oop/task2/command/Push.java
@@ -0,0 +1,40 @@
+package ru.nsu.fit.oop.task2.command;
+
+import ru.nsu.fit.oop.task2.Command;
+import ru.nsu.fit.oop.task2.Context;
+import ru.nsu.fit.oop.task2.command.exception.ArgumentsNumberException;
+import ru.nsu.fit.oop.task2.command.exception.ArgumentsFormatException;
+import ru.nsu.fit.oop.task2.util.Variable;
+
+import java.util.List;
+
+public class Push
+    implements Command
+{
+    private String value;
+
+    @Override
+    public void setArgs(List <String> args)
+    {
+        if (args.size() != 1)
+            throw new ArgumentsNumberException();
+
+        value = args.get(0);
+    }
+
+    @Override
+    public void run(Context context)
+    {
+        if (Variable.isCorrectVariableName(value))
+            context.pushCalculatingValue(context.getVariable(value));
+        else
+            try
+            {
+                context.pushCalculatingValue(Double.valueOf(value));
+            }
+            catch (NumberFormatException ex)
+            {
+                throw new ArgumentsFormatException();
+            }
+    }
+}
---
+++ src/test/java/ru/nsu/fit/oop/task2/command/PushTest.java
@@ -0,0 +1,77 @@
+package ru.nsu.fit.oop.task2.command;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.nsu.fit.oop.task2.Context;
+import ru.nsu.fit.oop.task2.command.exception.ArgumentsNumberException;
+import ru.nsu.fit.oop.task2.command.exception.ArgumentsFormatException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class PushTest
+{
+    private final Context context = new Context();
+    private final Push pushCmd = new Push();
+
+    private static Stream <Arguments> doesNotThrowTestArgs()
+    {
+        return Stream.of(
+                Arguments.of(new ArrayList <>(List.of("1.57")),
+                             1.57),
+                Arguments.of(new ArrayList<>(List.of("1")),
+                             1.0),
+                Arguments.of(new ArrayList<>(List.of("-3.54")),
+                             -3.54),
+                Arguments.of(new ArrayList<>(List.of("3.5E-01")),
+                             0.35),
+                Arguments.of(new ArrayList<>(List.of(".5")),
+                             0.5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("doesNotThrowTestArgs")
+    void doesNotThrowTest(ArrayList<String> args, Double expected)
+    {
+        Assertions.assertDoesNotThrow(() ->
+                                      {
+                                          pushCmd.setArgs(args);
+                                          pushCmd.run(context);
+                                      });
+
+        Assertions.assertEquals(expected,
+                                context.popCalculatingValue());
+    }
+
+    private static Stream<Arguments> throwTestArgs()
+    {
+        return Stream.of(
+                Arguments.of(new ArrayList<>(),
+                             ArgumentsNumberException.class),
+                Arguments.of(new ArrayList<>(List.of("a", "1.986")),
+                             ArgumentsNumberException.class),
+                Arguments.of(new ArrayList<>(List.of("a-1")),
+                             ArgumentsFormatException.class),
+                Arguments.of(new ArrayList<>(List.of("1.2F-01")),
+                             ArgumentsFormatException.class),
+                Arguments.of(new ArrayList<>(List.of("1.2az")),
+                             ArgumentsFormatException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("throwTestArgs")
+    void throwTest(ArrayList<String> args, Class<? extends Throwable> exceptionClass)
+    {
+        Assertions.assertThrows(exceptionClass, () ->
+            {
+                pushCmd.setArgs(args);
+                pushCmd.run(context);
+            }
+        );
+    }
+}
